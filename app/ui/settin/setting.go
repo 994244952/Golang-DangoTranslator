e219eeaa243ef5d482aa583bacdf2dac7794e3e4
PantsuDango <394883561@qq.com>
2021-06-22 01:08:10 +0800

    setting ui design

--- app/ui/settin/setting.go
+++ app/ui/settin/setting.go
@@ -12,8 +12,7 @@ func UI() *widgets.QMainWindow {
 	// 窗口标题
 	window.SetWindowTitle("团子翻译器 Ver4.0 - 设置")
 	// 窗口尺寸, 最大最小尺寸
-	window.Resize2(404, 576)
-
+	window.Resize2(600, 576)
 	// 窗口样式
 	window.SetStyleSheet(`QWidget { font: 10pt \"华康方圆体W7\";
                                                background-image: url(config/static/背景.jpg);
@@ -32,9 +31,9 @@ func UI() *widgets.QMainWindow {
 
 	// 顶部工具栏
 	tabWidget := widgets.NewQTabWidget(window)
-	tabWidget.SetGeometry2(-2, 0, 410, 580)
+	tabWidget.SetGeometry2(-2, 0, 600, 580)
 	tabWidget.SetCurrentIndex(0)
-	tabWidget.SetStyleSheet(`QTabBar::tab {min-width: 75px; background: rgba(255, 255, 255, 1);}
+	tabWidget.SetStyleSheet(`QTabBar::tab {min-width: 90px; background: rgba(255, 255, 255, 1);}
                                         QTabBar::tab:selected {border-bottom: 2px solid #4796f0;}
                                         QLabel{"background: transparent;}
                                         QCheckBox{"background: transparent;}`)
@@ -42,19 +41,34 @@ func UI() *widgets.QMainWindow {
 
 	// 工具栏1
 	tab_1 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
-	tabWidget.AddTab2(tab_1, gui.NewQIcon5("config/static/图标.ico"), "")
+	tabWidget.AddTab(tab_1, "")
 	tabWidget.SetTabText(tabWidget.IndexOf(tab_1), "API设定")
 
 	// 工具栏2
 	tab_2 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
-	tabWidget.AddTab2(tab_2, gui.NewQIcon5("config/static/图标.ico"), "")
+	tabWidget.AddTab(tab_2, "")
 	tabWidget.SetTabText(tabWidget.IndexOf(tab_2), "翻译源设定")
 
+	// 工具栏2
+	tab_3 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
+	tabWidget.AddTab(tab_3, "")
+	tabWidget.SetTabText(tabWidget.IndexOf(tab_3), "翻译样式")
+
+	// 工具栏2
+	tab_4 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
+	tabWidget.AddTab(tab_4, "")
+	tabWidget.SetTabText(tabWidget.IndexOf(tab_4), "其他设定")
+
+	// 工具栏2
+	tab_5 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
+	tabWidget.AddTab(tab_5, "")
+	tabWidget.SetTabText(tabWidget.IndexOf(tab_5), "支持作者")
+
 	// 同步配置按钮
-	getSettinButton := widgets.NewQPushButton(tab_1)
-	getSettinButton.SetGeometry2(324, 0, 80, 30)
-	getSettinButton.SetStyleSheet("background: rgba(255, 255, 255, 0.4);")
-	getSettinButton.SetText("同步设置")
+	getSettingButton := widgets.NewQPushButton(tab_1)
+	getSettingButton.SetGeometry2(324, 0, 80, 30)
+	getSettingButton.SetStyleSheet("background: rgba(255, 255, 255, 0.4);")
+	getSettingButton.SetText("同步设置")
 
 	window.SetLayout(layout)
 
